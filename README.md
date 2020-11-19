29a1760beb80f40eafb106758191ecd6b6157e59
Andy Kobyljanec <kobyl125@gmail.com>
2020-11-19 14:27:58 -0500

    Created docker-based deployment setup

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-# SmartThings InfluxDB Generator
+# SmartThings InfluxDB Grafana Application
 
-This script will run once and write influx data to a local instance.  This data can then be recorded and plotted over time with a tool such as Grafana:
+This application uses **Docker-Compose** to create an application which logs all of your Samsung Smart Things sensors into an Influx database and plots it on Grafana.
 
 ![Temperatureplot](doc/temperature_plot.png)
 
-## Python setup
-
-1. Create a virtual environment: `python -m venv /path/to/env`
-2. Activate the virtual environment `source /path/to/env/bin/activate`
-3. Install all dependencies `pip install -r requirements.txt`
 
 ## SmartThings API Key
 
@@ -19,16 +14,17 @@ to device statuses and devise lists
 2. Create a new token at [Samsung Personal Access Tokens](https://account.smartthings.com/tokens)
 3. Create a token with permissions to  (devices) list all devices, see all devices, (profiles) see all device profiles, and see locations
 
-## InfluxDB Setup
+## Docker Deployment
 
-Create a new user and database in InfluxDB for this script: [Influx Authentication and Authorization](https://docs.influxdata.com/influxdb/v1.8/administration/authentication_and_authorization/)
+A `docker-compose.yml` file is provided to set up all the required containers.  
 
-The new Influx user should have permissions to read and write to only the new smartthings database.
+1. Install Docker on your system 
+2. Create a `.env` file with an environment variable `SMARTTHINGS_API_KEY` set to your API key
+3. Add environment variables for `INFLUX_VOLUME` and `GRAFANA_VOLUME` paths to local directories
+3. Run `docker-compose up -d`
 
-## Set up Cron for automation
+## Local Python Development setup
 
-If you want this script to run periodically, the quickest way is to set up the script as a new cron job.  Be sure to use the virtual environment python executable (if using virtual environments)
-
-For example, to run this script every 5 minutes add this line to `crontab -e`:
-
-`*/5 * * * * /home/myuser/smartthings/smartthings-env/bin/python /home/myuser/smartthings/smartthings_influx.py`
+1. Create a virtual environment: `python -m venv /path/to/env`
+2. Activate the virtual environment `source /path/to/env/bin/activate`
+3. Install all dependencies `pip install -r requirements.txt`
--- smartthings_influx.py
+++ smartthings_influx.py
@@ -1,49 +1,34 @@
+import schedule
 import requests
-import configparser
 from influxdb import InfluxDBClient
 import logging
 from logging.handlers import RotatingFileHandler
 import os
-
-# Set up configparser to read config.ini in this directory
-config = configparser.RawConfigParser()
-config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
-config.read('config.ini')
+import time
 
 # SmartThings expects an "Authentication" header with the value
 # "Bearer: <api key>"
-KEY = 'Bearer ' + config.get('smartthings', 'smartthings_api_key')
+SMARTTHINGS_API_KEY = os.environ["SMARTTHINGS_API_KEY"]
+KEY = "Bearer " + SMARTTHINGS_API_KEY
 
 # Influx database, user, and password
-INFLUX_USER = config.get('influx', 'user')
-INFLUX_PASSWORD = config.get('influx', 'password')
-INFLUX_DATABASE = config.get('influx', 'database')
+INFLUX_NAME = os.environ["INFLUX_NAME"]
+INFLUX_DATABASE = os.environ["INFLUX_DATABASE"]
 
-# Set up rotating file logging
-LOG_NAME = config.get('logging', 'filename')
-LOG_MAXBYTES = int(config.get('logging', 'maxBytes'))
-LOG_BACKUPCOUNT = int(config.get('logging', 'backupCount'))
-LOG_FORMAT = '%(asctime)-15s %(message)s'
-
-logger = logging.getLogger('Logger')
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter(LOG_FORMAT)
-handler = RotatingFileHandler(
-    LOG_NAME, maxBytes=LOG_MAXBYTES, backupCount=LOG_BACKUPCOUNT)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+LOG_FORMAT = "%(asctime)-15s %(message)s"
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 
 
 class Device(object):
     def __init__(self):
-        self.name = ''
-        self.label = ''
-        self.id = ''
-        self.device_type_name = ''
+        self.name = ""
+        self.label = ""
+        self.id = ""
+        self.device_type_name = ""
 
 
 class buttonDevice(Device):
-    this_device_type_name = 'SmartSense Button'
+    this_device_type_name = "SmartSense Button"
     status_tags = []
 
     def __init__(self):
@@ -55,25 +40,22 @@ class buttonDevice(Device):
         button_data = [
             {
                 "measurement": "button",
-                "tags": {
-                    "label": f"{self.label}"
-                },
+                "tags": {"label": f"{self.label}"},
                 "fields": {
                     "temperature": self.temperature,
                     "battery": self.battery,
-                }
+                },
             }
         ]
-        client = InfluxDBClient('localhost', 8086, username=INFLUX_USER,
-                                password=INFLUX_PASSWORD, database=INFLUX_DATABASE)
+        client = InfluxDBClient(INFLUX_NAME, 8086, database=INFLUX_DATABASE)
         client.write_points(button_data)
-        logger.info(f'Wrote button data to influx: {self.label}')
+        logging.info(f"Wrote button data to influx: {self.label}")
 
 
 class waterLeakDevice(Device):
-    status_tags = ['temperatureMeasurement', 'battery', 'waterSensor']
-    commands = ['configuration', 'refresh', 'healthCheck']
-    this_device_type_name = 'SmartSense Moisture Sensor'
+    status_tags = ["temperatureMeasurement", "battery", "waterSensor"]
+    commands = ["configuration", "refresh", "healthCheck"]
+    this_device_type_name = "SmartSense Moisture Sensor"
 
     def __init__(self):
         self.water = False
@@ -85,26 +67,28 @@ class waterLeakDevice(Device):
         water_leak_data = [
             {
                 "measurement": "water_leak",
-                "tags": {
-                    "label": f"{self.label}"
-                },
+                "tags": {"label": f"{self.label}"},
                 "fields": {
                     "waterLeak": self.water,
                     "temperature": self.temperature,
                     "battery": self.battery,
-                }
+                },
             }
         ]
-        client = InfluxDBClient('localhost', 8086, username=INFLUX_USER,
-                                password=INFLUX_PASSWORD, database=INFLUX_DATABASE)
+        client = InfluxDBClient(INFLUX_NAME, 8086, database=INFLUX_DATABASE)
         client.write_points(water_leak_data)
-        logger.info(f'Wrote water leak data to influx: {self.label}')
+        logging.info(f"Wrote water leak data to influx: {self.label}")
 
 
 class multiDevice(Device):
-    this_device_type_name = 'SmartSense Multi Sensor'
-    status_tags = ['battery', 'temperature',
-                   'contactSensor', 'accelerationSensor', 'threeAxis']
+    this_device_type_name = "SmartSense Multi Sensor"
+    status_tags = [
+        "battery",
+        "temperature",
+        "contactSensor",
+        "accelerationSensor",
+        "threeAxis",
+    ]
 
     def __init__(self):
         self.battery = 0
@@ -118,23 +102,20 @@ class multiDevice(Device):
         multi_sensor_data = [
             {
                 "measurement": "multi_sensor",
-                "tags": {
-                    "label": f"{self.label}"
-                },
+                "tags": {"label": f"{self.label}"},
                 "fields": {
                     "temperature": self.temperature,
                     "battery": self.battery,
-                }
+                },
             }
         ]
-        client = InfluxDBClient('localhost', 8086, username=INFLUX_USER,
-                                password=INFLUX_PASSWORD, database=INFLUX_DATABASE)
+        client = InfluxDBClient(INFLUX_NAME, 8086, database=INFLUX_DATABASE)
         client.write_points(multi_sensor_data)
-        logger.info(f'Wrote multisensor data to influx: {self.label}')
+        logging.info(f"Wrote multisensor data to influx: {self.label}")
 
 
 class powerOutlet(Device):
-    this_device_type_name = 'SmartPower Outlet'
+    this_device_type_name = "SmartPower Outlet"
     status_tags = []
 
     def __init__(self):
@@ -145,18 +126,15 @@ class powerOutlet(Device):
         power_outlet_data = [
             {
                 "measurement": "power_outlet",
-                "tags": {
-                    "label": f"{self.label}"
-                },
+                "tags": {"label": f"{self.label}"},
                 "fields": {
                     "power": self.power,
-                }
+                },
             }
         ]
-        client = InfluxDBClient('localhost', 8086, username=INFLUX_USER,
-                                password=INFLUX_PASSWORD, database=INFLUX_DATABASE)
+        client = InfluxDBClient(INFLUX_NAME, 8086, database=INFLUX_DATABASE)
         client.write_points(power_outlet_data)
-        logger.info(f'Wrote power data to influx: {self.label}')
+        logging.info(f"Wrote power data to influx: {self.label}")
 
 
 class temperatureMeasurement(object):
@@ -166,81 +144,106 @@ class temperatureMeasurement(object):
         self.unit = unit
 
 
-logger.info('Start running script')
-# get a list of all devices
-r = requests.get('https://api.smartthings.com/v1/devices',
-                 headers={'Authorization': KEY})
+def run_script():
+    logging.info("Start running script")
+    # get a list of all devices
+    r = requests.get(
+        "https://api.smartthings.com/v1/devices", headers={"Authorization": KEY}
+    )
 
-devices = r.json()['items']
-device_list = []
-influx_counted = 0
+    devices = r.json()["items"]
+    device_list = []
+    influx_counted = 0
 
-for device in devices:
-    new_device = Device()
-    new_device.name = device['name']
-    new_device.label = device['label']
-    new_device.id = device['deviceId']
-    # A hub will appear as a device, but it will not have a
-    # 'deviceTypeName' field so we can ignore it
-    if 'deviceTypeName' in device:
-        new_device.device_type_name = device['deviceTypeName']
-    else:
-        new_device.device_type_name = ''
+    for device in devices:
+        new_device = Device()
+        new_device.name = device["name"]
+        new_device.label = device["label"]
+        new_device.id = device["deviceId"]
+        # A hub will appear as a device, but it will not have a
+        # 'deviceTypeName' field so we can ignore it
+        if "deviceTypeName" in device:
+            new_device.device_type_name = device["deviceTypeName"]
+        else:
+            new_device.device_type_name = ""
 
-    # Get device status
-    status = requests.get(
-        f'https://api.smartthings.com/v1/devices/{new_device.id}/status', headers={'Authorization': KEY})
-    status_json = status.json()
+        # Get device status
+        status = requests.get(
+            f"https://api.smartthings.com/v1/devices/{new_device.id}/status",
+            headers={"Authorization": KEY},
+        )
+        status_json = status.json()
 
-    if new_device.device_type_name == waterLeakDevice.this_device_type_name:
-        water_leak_sensor = waterLeakDevice()
-        water_leak_sensor.label = new_device.label
-        try:
-            water_leak_sensor.battery = status_json['components']['main']['battery']['battery']['value']
-            water_leak_sensor.temperature = status_json['components'][
-                'main']['temperatureMeasurement']['temperature']['value']
-            water_string = status_json['components']['main']['waterSensor']['water']['value']
-            if water_string == 'dry':
-                water_leak_sensor.water = False
-            else:
-                water_leak_sensor.water = True
-            water_leak_sensor.send_data()
-            influx_counted += 1
-        except:
-            logger.warning('failed water sensor')
-    elif new_device.device_type_name == buttonDevice.this_device_type_name:
-        button_device = buttonDevice()
-        button_device.label = new_device.label
-        try:
-            button_device.battery = status_json['components']['main']['battery']['battery']['value']
-            button_device.temperature = status_json['components'][
-                'main']['temperatureMeasurement']['temperature']['value']
-            button_device.send_data()
-            influx_counted += 1
-        except:
-            logger.warning('failed button sensor')
-    elif new_device.device_type_name == multiDevice.this_device_type_name:
-        multi_device = multiDevice()
-        multi_device.label = new_device.label
-        try:
-            multi_device.battery = status_json['components']['main']['battery']['battery']['value']
-            multi_device.temperature = status_json['components'][
-                'main']['temperatureMeasurement']['temperature']['value']
-            multi_device.send_data()
-            influx_counted += 1
-        except:
-            logger.warning('failed multi sensor')
-    elif new_device.device_type_name == powerOutlet.this_device_type_name:
-        outlet_device = powerOutlet()
-        outlet_device.label = new_device.label
-        try:
-            outlet_device.power = status_json['components']['main']['powerMeter']['power']['value']
-            outlet_device.send_data()
-            influx_counted += 1
-        except:
-            logger.warning('failed outlet sensor')
+        if new_device.device_type_name == waterLeakDevice.this_device_type_name:
+            water_leak_sensor = waterLeakDevice()
+            water_leak_sensor.label = new_device.label
+            try:
+                water_leak_sensor.battery = status_json["components"]["main"][
+                    "battery"
+                ]["battery"]["value"]
+                water_leak_sensor.temperature = status_json["components"]["main"][
+                    "temperatureMeasurement"
+                ]["temperature"]["value"]
+                water_string = status_json["components"]["main"]["waterSensor"][
+                    "water"
+                ]["value"]
+                if water_string == "dry":
+                    water_leak_sensor.water = False
+                else:
+                    water_leak_sensor.water = True
+                water_leak_sensor.send_data()
+                influx_counted += 1
+            except:
+                logging.warning("failed water sensor")
+        elif new_device.device_type_name == buttonDevice.this_device_type_name:
+            button_device = buttonDevice()
+            button_device.label = new_device.label
+            try:
+                button_device.battery = status_json["components"]["main"]["battery"][
+                    "battery"
+                ]["value"]
+                button_device.temperature = status_json["components"]["main"][
+                    "temperatureMeasurement"
+                ]["temperature"]["value"]
+                button_device.send_data()
+                influx_counted += 1
+            except:
+                logging.warning("failed button sensor")
+        elif new_device.device_type_name == multiDevice.this_device_type_name:
+            multi_device = multiDevice()
+            multi_device.label = new_device.label
+            try:
+                multi_device.battery = status_json["components"]["main"]["battery"][
+                    "battery"
+                ]["value"]
+                multi_device.temperature = status_json["components"]["main"][
+                    "temperatureMeasurement"
+                ]["temperature"]["value"]
+                multi_device.send_data()
+                influx_counted += 1
+            except:
+                logging.warning("failed multi sensor")
+        elif new_device.device_type_name == powerOutlet.this_device_type_name:
+            outlet_device = powerOutlet()
+            outlet_device.label = new_device.label
+            try:
+                outlet_device.power = status_json["components"]["main"]["powerMeter"][
+                    "power"
+                ]["value"]
+                outlet_device.send_data()
+                influx_counted += 1
+            except:
+                logging.warning("failed outlet sensor")
 
-    device_list.append(new_device)
+        device_list.append(new_device)
 
-logger.info(
-    f'Completed run with {influx_counted}/{len(devices) - 1} sensors uplaoded successfully')
+    logging.info(
+        f"Completed run with {influx_counted}/{len(devices) - 1} sensors uploaded successfully"
+    )
+
+
+schedule.every(10).minutes.do(run_script)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
